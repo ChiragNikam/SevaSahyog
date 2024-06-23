242b6657389a70910fdaf1ef9889f298e08ac6cd
Rajat Kumar singh <rajatkumarsingh@Rajats-MacBook-Air.local>
2024-06-23 20:55:53 +0530

    api calling -  create events

--- app/src/main/java/com/learn/sevasahyog/ngo_home/items/event/domain/CreateEventViewModel.kt
+++ app/src/main/java/com/learn/sevasahyog/ngo_home/items/event/domain/CreateEventViewModel.kt
@@ -1,11 +1,20 @@
 package com.learn.sevasahyog.ngo_home.items.event.domain
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonSyntaxException
+import com.learn.sevasahyog.auth.data.dataclass.ErrorResponse
+import com.learn.sevasahyog.network.RetrofitInstance
 import com.learn.sevasahyog.ngo_home.data.NgoAccount
+import com.learn.sevasahyog.ngo_home.data.NgoService
 import com.learn.sevasahyog.ngo_home.items.event.data.CreateEvent
 import com.learn.sevasahyog.ngo_home.items.event.data.EventRequest
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import retrofit2.create
 
 class EventViewModel : ViewModel() {
 
@@ -58,11 +67,50 @@ class EventViewModel : ViewModel() {
         this._event.value.longDesc = eventLongDesc
     }
 
+    // access token
+    private val _accessToken = MutableStateFlow("")
+    val accessToken get() = _accessToken
+
+    fun updateAccessToken(token: String){
+        _accessToken.value = token
+    }
     private val _createEvent = MutableStateFlow(CreateEvent())
     val createEvent get() = _createEvent
 
     private val _event = MutableStateFlow(EventRequest())
     val event get() = _event
 
+    // Function to create event
+    fun createEvent() {
+        viewModelScope.launch {
+            try {
+                val ngoService = RetrofitInstance.getClient("http://192.168.43.231:8080/").create(NgoService::class.java)
+                val eventResponse = ngoService.createEvent(token = "Bearer ${_accessToken.value}", createEventBody = _createEvent.value)
 
+                if (eventResponse.isSuccessful) {
+                    _event.value = (eventResponse.body() ?: EventRequest()) as EventRequest
+                } else {
+                    val errorBody = eventResponse.errorBody()?.string()
+                    Log.d("error_response_raw", errorBody ?: "No error body")
+
+                    val gson = GsonBuilder().setLenient().create()
+                    val errorResponse: ErrorResponse? = errorBody?.let {
+                        try {
+                            gson.fromJson(it, ErrorResponse::class.java)
+                        } catch (e: JsonSyntaxException) {
+                            Log.e("GsonError", "Error parsing JSON", e)
+                            null
+                        }
+                    }
+
+                    Log.d("error_response", eventResponse.code().toString())
+                    Log.d("error_response_body", errorResponse?.errorMessage ?: "No error message")
+                }
+            } catch (e: Exception) {
+                Log.e("NetworkError", "Error creating event", e)
+            }
+        }
+    }
 }
+
+
